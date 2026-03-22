6552d2a73d5b1e25906bc339e62edc2b81481910
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-03-23 00:57:18 +0200

    Time: 4 ms (66.32%), Space: 45.8 MB (71.42%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s == null || t == null || s.length() < t.length()) return "";
+
+        int[] haveArr = new int[128];
+        int have = 0;
+        for(char c : t.toCharArray()){
+            haveArr[c]++;
+            if(haveArr[c] == 1) have++;
+        }
+
+        int[] needArr = new int[128];
+        int need = 0;
+        int minLength = Integer.MAX_VALUE;
+        int left = 0;
+        int l = 0 , r = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            needArr[s.charAt(i)]++;
+            if(needArr[s.charAt(i)] == haveArr[s.charAt(i)]) need++;
+
+            while(have == need){
+                if(i - left + 1 < minLength){
+                    minLength = i - left + 1;
+                    l=left ; r=i+1;
+                }
+                if(needArr[s.charAt(left)] == haveArr[s.charAt(left)]) need--;
+                needArr[s.charAt(left++)]--;
+            }
+        }
+        return s.substring(l,r);
+    }
+}
