263f32f1a0b816b334fec3183dfa60aaa2c3fa5d
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-04-09 10:59:45 +0200

    Time: 1 ms (100%), Space: 46.6 MB (26.7%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,20 +11,23 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0){
-            int l1Val = (l1 == null) ? 0 : l1.val; // 9 , 9 , 9 , 9 , 9 , 9
-            int l2Val = (l2 == null) ? 0 : l2.val; // 9 , 9 , 9 , 9 , 0 , 0
-            int sum = l1Val + l2Val + carry; // 18 , 18 , 18  , 18 , 9
-            curr.next = new ListNode((sum) % 10); // 8 , 9 , 9 , 9 , 0 , 0
-            carry = sum / 10; // 1 , 1 , 1 , 1 , 1 , 1
+        ListNode l3 = dummy;
 
-            if(l1 != null) l1 = l1.next; // 9 , 9 , 9 , 9 , 9 , 9
-            if(l2 != null) l2 = l2.next; // 9 , 9 , 9 , 9 , null
-            curr = curr.next;
+        int cin = 0; 
+        while(l1 != null || l2 != null || cin != 0){
+            int fir = (l1 != null) ? l1.val : 0;
+            int sec = (l2 != null) ? l2.val : 0;
+
+            int sum = fir + sec + cin;
+            cin = sum / 10;
+
+            l3.next = new ListNode(sum % 10);
+            l3 = l3.next;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }
-        
+
         return dummy.next;
     }
 }
