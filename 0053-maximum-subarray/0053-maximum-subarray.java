124d3d53cc29b9e40011e25e222f27e41572ea1f
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-04-09 21:53:35 +0200

    Time: 1 ms (99.98%), Space: 77.3 MB (38.48%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            currentSum = Math.max(currentSum , 0);
+            currentSum += nums[i];
+            max = Math.max(max , currentSum);
+        }
+        
+        return max;
+    }
+}
