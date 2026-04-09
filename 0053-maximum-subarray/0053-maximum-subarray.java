f779fc1e83b7bdc6f34100005085dca2f43ecff7
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-04-09 21:57:22 +0200

    Time: 1 ms (99.98%), Space: 77.2 MB (72.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,11 +4,10 @@ class Solution {
         int max = Integer.MIN_VALUE;
 
         for(int i = 0 ; i < nums.length ; i++){
-            currentSum = Math.max(currentSum , 0);
-            currentSum += nums[i];
+            currentSum = Math.max(nums[i] , currentSum + nums[i]);
             max = Math.max(max , currentSum);
         }
-        
+
         return max;
     }
 }
