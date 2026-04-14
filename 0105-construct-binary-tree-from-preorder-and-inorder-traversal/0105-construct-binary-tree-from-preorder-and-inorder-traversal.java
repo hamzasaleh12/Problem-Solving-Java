d3c527c355de79b8c0767191250db40b0c9c60af
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-04-14 14:17:02 +0200

    Time: 3 ms (29.51%), Space: 46.4 MB (55.04%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,25 +14,22 @@
  * }
  */
 class Solution {
-    Map<Integer,Integer> map = new HashMap<>();
-    
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        for(int i = 0 ; i < inorder.length ; i++){
-            map.put(inorder[i] , i);
-        }
-        return helper(0 , 0 , inorder.length - 1 , preorder);
-    }
-    private TreeNode helper(int preStart ,int inStart , int inEnd ,int[] preorder){
-        if(inStart > inEnd) return null;
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int i = 0 ; i < inorder.length ; i++) map.put(inorder[i] , i);
 
+        return helper(0 , preorder.length - 1 , 0 , inorder.length - 1 , preorder , map);
+    }
+    private TreeNode helper(int preStart , int preEnd , int inStart , int inEnd , int[] preorder , Map<Integer , Integer> map){
+        if(preStart > preEnd || inStart > inEnd) return null;
         TreeNode root = new TreeNode(preorder[preStart]);
 
-        int mid = map.get(root.val);
-        int leftSize = mid - inStart;
+        int inRoot = map.get(root.val);
+        int leftSize = inRoot - inStart;
 
-        root.left = helper(preStart + 1 , inStart , mid - 1 , preorder);
-        root.right = helper(preStart + leftSize + 1 , mid + 1, inEnd , preorder);
-        
+        root.left = helper(preStart + 1 , preStart + leftSize , inStart , inRoot - 1 , preorder , map);
+        root.right = helper(preStart + leftSize + 1 , preEnd , inRoot + 1 , inEnd , preorder , map);
+
         return root;
     }
 }
