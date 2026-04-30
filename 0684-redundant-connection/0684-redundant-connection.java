e304058ab1353a6e08add4fd1950ffa3a531a43b
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-04-30 13:43:22 +0300

    Time: 1 ms (90.78%), Space: 44.7 MB (93.9%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int[] parents = new int[edges.length + 1];
+        int[] rank = new int[edges.length + 1];
         for(int i = 1 ; i <= edges.length ; i++){
             parents[i] = i; // 1 -> 1 , ...
         }
@@ -9,7 +10,7 @@ class Solution {
             int firPar = find(edge[0] , parents);
             int secPar = find(edge[1] , parents);
 
-            if(firPar != secPar) union(firPar , secPar , parents);
+            if(firPar != secPar) union(firPar , secPar , parents , rank);
 
             else return new int[]{edge[0] , edge[1]};
         }
@@ -20,9 +21,10 @@ class Solution {
     private int find(int val , int[] parents){ // 5 , parents  , 3
         if(parents[val] == val) return val; // 3 = 5 ? f , 2 = 3 ? f , 1 = 2 ? f , 1 = 1 ? t
 
-        return find(parents[val] , parents); // 3 , 2 , 1
+        return parents[val] = find(parents[val] , parents); // كاني بعمل كاشينج للمعلومه
     }
-    private void union(int fir , int sec , int[] parents){
-        parents[sec] = fir;
+    private void union(int fir , int sec , int[] parents , int[] rank){
+        if(rank[fir] >= rank[sec]) parents[sec] = fir;
+        else parents[fir] = sec;
     }
 }
