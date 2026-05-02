716593189435a8d5d2d16d9505e1d1d2209ff0d2
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-02 13:28:21 +0300

    Time: 9 ms (96.11%), Space: 49.1 MB (55.27%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,46 +1,40 @@
 class Solution {
-    class Data{
-        int node;
-        int dis;
-        public Data(int node , int dis) {
-            this.node = node;
-            this.dis = dis;
-        }
-    }
+    record Data(int node, int dis) {}
+
     public int networkDelayTime(int[][] times, int n, int k) {
         int[] distance = new int[n + 1];
-        for(int i = 1 ; i < n + 1 ; i++){
-            if(i == k) continue;
-            distance[i] = Integer.MAX_VALUE;
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[k] = 0;
+
+        List<int[]>[] adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) adj[i] = new ArrayList<>();
+        
+        for (int[] time : times) {
+            adj[time[0]].add(new int[]{time[1], time[2]});
         }
-        Map<Integer,List<int[]>> map = new HashMap<>();
-        for(int[] time : times){
-            map.computeIfAbsent(time[0] , key -> new ArrayList<>()).add(new int[]{time[1] , time[2]});
-        }
 
-        PriorityQueue<Data> queue = new PriorityQueue<>((a , b) -> Integer.compare(a.dis , b.dis));
-        queue.add(new Data(k , distance[k])); // (2,0)
+        PriorityQueue<Data> queue = new PriorityQueue<>((a, b) -> Integer.compare(a.dis, b.dis));
+        queue.add(new Data(k, 0));
 
-        while(!queue.isEmpty()){
-            Data t = queue.poll(); // (node:2,dis:0) , 1 , 3
-            if(distance[t.node] < t.dis) continue;
+        while (!queue.isEmpty()) {
+            Data t = queue.poll();
+            if (distance[t.node] < t.dis) continue;
 
-            for(int[] neighbor : map.getOrDefault(t.node , new ArrayList<>())){
-                // Relaxation
-                int targetNode = neighbor[0]; // 1 , 3 , 4
-                int disEdge = neighbor[1];//2 -'1'-> 1 , 2 -'1'-> 3 , 1
+            for (int[] neighbor : adj[t.node]) {
+                int targetNode = neighbor[0];
+                int disEdge = neighbor[1];
 
-                if(t.dis + disEdge < distance[targetNode]){ // 0 + 1 < INF , 0 + 1 < INF
-                    distance[targetNode] = t.dis + disEdge; // dis[1] = 1 , dis[3] = 1
-                    queue.add(new Data(targetNode , distance[targetNode])); // 1 , 3
+                if (t.dis + disEdge < distance[targetNode]) {
+                    distance[targetNode] = t.dis + disEdge;
+                    queue.add(new Data(targetNode, distance[targetNode]));
                 }
             }
         }
 
-        int max = Integer.MIN_VALUE;
-        for(int dis : distance){
-            if(dis == Integer.MAX_VALUE) return -1;
-            max = Math.max(max , dis);
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (distance[i] == Integer.MAX_VALUE) return -1;
+            max = Math.max(max, distance[i]);
         }
 
         return max;
