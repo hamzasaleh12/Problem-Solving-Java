402adb3d0828c46779c172cff7a868263ba0d100
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-02 12:56:28 +0300

    Time: 13 ms (47.26%), Space: 49.1 MB (55.27%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -13,6 +13,10 @@ class Solution {
             if(i == k) continue;
             distance[i] = Integer.MAX_VALUE;
         }
+        Map<Integer,List<int[]>> map = new HashMap<>();
+        for(int[] time : times){
+            map.computeIfAbsent(time[0] , key -> new ArrayList<>()).add(new int[]{time[1] , time[2]});
+        }
 
         PriorityQueue<Data> queue = new PriorityQueue<>((a , b) -> Integer.compare(a.dis , b.dis));
         queue.add(new Data(k , distance[k])); // (2,0)
@@ -21,17 +25,14 @@ class Solution {
             Data t = queue.poll(); // (node:2,dis:0) , 1 , 3
             if(distance[t.node] < t.dis) continue;
 
-            for(int[] time : times){
-                int srcNode = time[0];//2 , 2 , 3
+            for(int[] neighbor : map.getOrDefault(t.node , new ArrayList<>())){
+                // Relaxation
+                int targetNode = neighbor[0]; // 1 , 3 , 4
+                int disEdge = neighbor[1];//2 -'1'-> 1 , 2 -'1'-> 3 , 1
 
-                if(t.node == srcNode){ // same src(2) t , t , f , f, f, ,f t
-                    // Relaxation
-                    int targetNode = time[1]; // 1 , 3 , 4
-                    int disEdge = time[2];//2 -'1'-> 1 , 2 -'1'-> 3 , 1
-                    if(t.dis + disEdge < distance[targetNode]){ // 0 + 1 < INF , 0 + 1 < INF
-                        distance[targetNode] = t.dis + disEdge; // dis[1] = 1 , dis[3] = 1
-                        queue.add(new Data(targetNode , distance[targetNode])); // 1 , 3
-                    }
+                if(t.dis + disEdge < distance[targetNode]){ // 0 + 1 < INF , 0 + 1 < INF
+                    distance[targetNode] = t.dis + disEdge; // dis[1] = 1 , dis[3] = 1
+                    queue.add(new Data(targetNode , distance[targetNode])); // 1 , 3
                 }
             }
         }
