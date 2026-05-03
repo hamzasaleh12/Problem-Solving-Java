6e0173cfd3e1a65ccc030386e7b94a7caa807697
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-03 22:01:03 +0300

    Time: 2235 ms (5%), Space: 74.6 MB (5.02%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,15 +1,31 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> Integer.compare(b , a));
-        for(int num : nums){
-            queue.add(num);
-        }
+        return recQuickSelect(nums , 0 , nums.length - 1 , k);
+    }
+    private int recQuickSelect(int[] nums , int start , int end ,int k){
+        int mid =  partition(nums , start , end);
+        int tar = nums.length - k;
 
-        while(k > 1){
-            queue.poll();
-            k--;
-        }
+        if(mid == tar) return nums[mid];
+        else if(mid < tar) return recQuickSelect(nums , mid + 1 , end , k);
+        else return recQuickSelect(nums , start , mid - 1 , k);
+    }
+    private int partition(int[] nums , int start , int end){
+        int i = start - 1;
+        int pivot = nums[end];
 
-        return queue.poll();
+        for(int j = start ; j < end ; j++){
+            if(nums[j] < pivot){
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        i++;
+        int temp = nums[i];
+        nums[i] = nums[end];
+        nums[end] = temp;
+        return i;
     }
 }
