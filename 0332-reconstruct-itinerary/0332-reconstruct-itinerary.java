304fe0fdfdc3b43d624823096ac1b3a4757ed40c
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-09 16:33:13 +0300

    Time: 6 ms (80.7%), Space: 47.2 MB (49.4%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String , PriorityQueue<String>> map = new HashMap<>();
+        for(List<String> ticket : tickets){
+            map.computeIfAbsent(ticket.get(0) , k -> new PriorityQueue<>()).add(ticket.get(1));
+        }
+
+        List<String> res = new ArrayList<>();
+        
+        dfs("JFK" , map , res);
+
+        List<String> result = new ArrayList<>();
+        for(int i = res.size() - 1 ; i >= 0 ; i--){
+            result.add(res.get(i));
+        }
+        return result;
+    }
+    private void dfs(String city , Map<String , PriorityQueue<String>> map , List<String> res){
+        PriorityQueue<String> pq = map.get(city);
+
+        while(pq != null && !pq.isEmpty()){
+            String nextCity = map.get(city).poll();
+            dfs(nextCity , map , res);
+        }
+
+        res.add(city);
+    }
+}
