1c5630deafb8dcf1c26dd703f3be311cb779e02d
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-12 12:45:26 +0300

    Time: 3 ms (33.34%), Space: 45.9 MB (18.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -3,6 +3,7 @@ class Solution {
     List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         dfs(0 , candidates , target , 0);
         return res;
     }
@@ -11,6 +12,7 @@ class Solution {
             if(val == target) res.add(new ArrayList<>(subset));
             return;
         }
+        if(val + nums[i] > target) return;
 
         subset.add(nums[i]);
         dfs(i , nums , target , val + nums[i]);
