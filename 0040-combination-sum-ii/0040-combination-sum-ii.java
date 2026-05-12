07889f8d0b550990b4009c92affbdff0716d8df7
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-12 13:03:30 +0300

    Time: 7 ms (35.37%), Space: 45.6 MB (19.15%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<Integer> subset = new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        dfs(0 , candidates , target , 0);
+        return res;
+    }
+    private void dfs(int i , int[] nums , int target , int val){
+        if(i >= nums.length || val >= target){
+            if(val == target) res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        dfs(i + 1 , nums , target , val + nums[i]);
+
+        subset.removeLast();
+        while(i + 1 < nums.length && nums[i] == nums[i + 1]){
+            i++;
+        }
+        dfs(i + 1 , nums , target , val);
+    }
+}
