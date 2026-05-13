424da212a916e44c7252029c1c3428efdc417473
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-13 19:53:34 +0300

    Time: 132 ms (64.34%), Space: 43.2 MB (30.2%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,37 +1,37 @@
 class Solution {
-    private static final int[][] drs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-    private boolean isFound = false;
 
     public boolean exist(char[][] board, String word) {
         if(board == null || board.length == 0 || word == null) return false;
 
         int l = board.length; int w = board[0].length;
-        char start = word.charAt(0); // 'A'
-        boolean[][] visited = new boolean[l][w];
+        char start = word.charAt(0);
+
         for(int i = 0 ; i < l ; i++){
             for(int j = 0 ; j < w ; j++){
-                if(isFound) return true;
                 if(start == board[i][j]){
-                    dfs(board , i , j, word , 0 , visited);
+                    if(dfs(board , i , j, word , 0)){
+                        return true;
+                    }
                 }
             }
         }
-        return isFound;
-    }
-    private void dfs(char[][] board, int x , int y , String word , int idx , boolean[][] visited){
-        if(isFound) return;
-        if(idx == word.length()){
-            isFound = true;
-            return;
-        }
-        if(x >= board.length || x < 0 || y >= board[0].length || y < 0 || visited[x][y]) return;
 
-        if(board[x][y] == word.charAt(idx)){
-            visited[x][y] = true;
-            for(int[] d : drs){
-                dfs(board , x + d[0] , y + d[1] , word , idx + 1 , visited);
-            }
-            visited[x][y] = false;
+        return false;
+    }
+    private boolean dfs(char[][] board, int r , int c , String word , int idx){
+        if(idx == word.length()){
+            return true;
         }
+        if(r >= board.length || r < 0 || c >= board[0].length || c < 0 || board[r][c] == '#' || board[r][c] != word.charAt(idx)) return false;
+
+        char temp = board[r][c];
+        board[r][c] = '#';
+        boolean found = dfs(board, r + 1, c, word, idx + 1) ||
+                        dfs(board, r - 1, c, word, idx + 1) ||
+                        dfs(board, r, c + 1, word, idx + 1) ||
+                        dfs(board, r, c - 1, word, idx + 1);
+        board[r][c] = temp;
+
+        return found;
     }    
 }
