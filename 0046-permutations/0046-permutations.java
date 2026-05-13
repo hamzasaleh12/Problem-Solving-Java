4d765bd1358c437466df1fbd58a4b223838a7ba8
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-13 18:33:01 +0300

    Time: 1 ms (86.77%), Space: 45.6 MB (46.27%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,25 @@
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
-    List<Integer> permute = new ArrayList<>();
-    List<List<Integer>> res = new ArrayList<>();
-
     public List<List<Integer>> permute(int[] nums) {
-        dfs(nums);
+        List<Integer> permute = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        dfs(nums , permute , res , used);
         return res;
     }
-    private void dfs(int[] nums){
+    private void dfs(int[] nums , List<Integer> permute , List<List<Integer>> res , boolean[] used){
         if(nums.length == permute.size()){
             res.add(new ArrayList<>(permute));
             return;
         }
         for(int i = 0 ; i < nums.length ; i++){
-            if(!set.add(nums[i])) continue;
+            if(used[i]) continue;
 
+            used[i] = true;
             permute.add(nums[i]);
-            dfs(nums);
-            set.remove(permute.removeLast());
+            dfs(nums , permute , res , used);
+
+            used[i] = false;
+            permute.removeLast();
         }
     }
 }
