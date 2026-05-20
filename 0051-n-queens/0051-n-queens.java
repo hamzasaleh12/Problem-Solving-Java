b72d61c46bd6832430ca8a8a3898f770f7d985ba
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-20 23:25:50 +0300

    Time: 2 ms (81.63%), Space: 46.4 MB (90.83%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -3,23 +3,25 @@ class Solution {
         List<String> part = new ArrayList<>();
         List<List<String>> res = new ArrayList<>();
         boolean[] visited = new boolean[n];
+        boolean[] diag1 = new boolean[n * 2];
+        boolean[] diag2 = new boolean[n * 2];
 
-        backTrack(0 , n , part , res , visited , new HashSet<>() , new HashSet<>());
+        backTrack(0 , n , part , res , visited , diag1 , diag2);
         return res;
     }
-    private void backTrack(int row , int n , List<String> part , List<List<String>> res , boolean[] visited , Set<Integer> diag1 , Set<Integer> diag2){
+    private void backTrack(int row , int n , List<String> part , List<List<String>> res , boolean[] visited , boolean[] diag1 , boolean[] diag2){
         if(part.size() == n){
             res.add(new ArrayList<>(part));
             return;
         }
         for(int col = 0 ; col < n ; col++){
             int main = row + col;
-            int anti = row - col;
+            int anti = row - col + (n - 1);
 
-            if(!visited[col] && !diag1.contains(main) && !diag2.contains(anti)){
+            if(!visited[col] && !diag1[main] && !diag2[anti]){
                 visited[col] = true;
-                diag1.add(main);
-                diag2.add(anti);
+                diag1[main] = true;
+                diag2[anti] = true;
 
                 char[] charArr = new char[n];
                 Arrays.fill(charArr , '.');
@@ -30,8 +32,8 @@ class Solution {
 
                 part.removeLast();
                 visited[col] = false;
-                diag1.remove(main);
-                diag2.remove(anti);
+                diag1[main] = false;
+                diag2[anti] = false;
             }
         }
     }
