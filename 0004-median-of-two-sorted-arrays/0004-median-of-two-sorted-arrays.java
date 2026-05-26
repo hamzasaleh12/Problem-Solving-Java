367cfc44d2e52b03f8861a35af23b6a3862eee16
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-05-26 07:56:10 +0300

    Time: 1 ms (100%), Space: 48.8 MB (69.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,30 +2,29 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int l1 = (nums1 != null) ? nums1.length : 0;
         int l2 = (nums2 != null) ? nums2.length : 0;
-        if(l1 + l2 == 0) return 0;
-        if(l1 + l2 == 1) return (l1 == 1) ? nums1[0] : nums2[0];
+        int totalLength = l1 + l2;
 
+        int prev = 0;
+        int curr = 0;
 
-        int[] nums3 = new int[l1 + l2];
-        merge(nums1 , nums2 , nums3);
-        
-        int newLength = l1 + l2;
-        if(newLength % 2 != 0){
-            return nums3[newLength / 2];
-        } else{ // even
-            return (nums3[(newLength / 2) - 1] + nums3[(newLength / 2)]) / 2.0;
-        }
-    }
-    private void merge(int[] nums1 , int[] nums2 , int[] nums3){
-        int i = 0 , j = 0 , k = 0;
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] <= nums2[j]){
-                nums3[k++] = nums1[i++];
+        int i = 0;
+        int j = 0;
+        for(int count = 0 ; count <= totalLength / 2 ; count++){
+            prev = curr;
+            if (i < l1 && j < l2) {
+                if(nums1[i] <= nums2[j]){
+                    curr = nums1[i++];
+                } else{
+                    curr = nums2[j++];
+                }
             } else{
-                nums3[k++] = nums2[j++];
+                if(i < l1){
+                    curr = nums1[i++];
+                } else {
+                    curr = nums2[j++];
+                }
             }
         }
-        while(i < nums1.length) nums3[k++] = nums1[i++];
-        while(j < nums2.length) nums3[k++] = nums2[j++];
+        return (totalLength % 2 == 0) ? (prev + curr) / 2.0 : curr;
     }
 }
