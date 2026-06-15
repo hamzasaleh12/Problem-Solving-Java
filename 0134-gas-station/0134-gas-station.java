f802e537b6e5174158831572126e354f8cff1825
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-15 15:58:17 +0300

    Time: 1531 ms (5.15%), Space: 97.8 MB (99.79%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1] , b[1]));
+        int n = gas.length;
+        if(n == 1 && gas[0] == cost[0]) return 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(gas[i] <= cost[i]) continue;
+            int[] curr = new int[]{i , gas[i] - cost[i]};
+            pq.add(curr);
+        }
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int idx = curr[0]; // 3 -> {4}
+            boolean[] visited = new boolean[n];
+
+            int currIdx = idx; // 3
+            int currSum = gas[currIdx]; // 4
+            while(currSum >= cost[currIdx]){
+                if(visited[currIdx]) return idx;
+
+                visited[currIdx] = true; // t,t,t,t
+                int nextIdx = (currIdx != n - 1) ? currIdx + 1 : 0; // 4,0,1,2
+
+                currSum = currSum - cost[currIdx] + gas[nextIdx]; // 8,7,6,5
+                currIdx = nextIdx; // 4,0,1,2
+            }
+        }
+        return -1;
+    }
+}
