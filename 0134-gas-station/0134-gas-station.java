9216148bdae6361e832b039494115889a8b0baa9
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-15 16:38:50 +0300

    Time: 2 ms (99.37%), Space: 119.6 MB (9.22%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,32 +1,25 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[1] , a[1]));
         int n = gas.length;
-        if(n == 1 && gas[0] == cost[0]) return 0;
 
-        for(int i = 0 ; i < n ; i++){
-            if(gas[i] <= cost[i]) continue;
-            int[] curr = new int[]{i , gas[i] - cost[i]};
-            pq.add(curr);
+        int sumGas = 0;
+        int sumCost = 0;
+        for(int i = 0; i < n ; i++){
+            sumGas += gas[i];
+            sumCost += cost[i];
         }
+        if(sumCost > sumGas) return -1;
 
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int idx = curr[0]; // 3 -> {4}
-            int count = 0;
+        int total = 0;
+        int idx = 0;
+        for(int i = 0 ; i < n ; i++){
+            total += gas[i] - cost[i];
 
-            int currIdx = idx; // 3
-            int currSum = gas[currIdx]; // 4
-            while(currSum >= cost[currIdx]){
-                if(count == n) return idx;
-
-                count++; // t,t,t,t
-                int nextIdx = (currIdx != n - 1) ? currIdx + 1 : 0; // 4,0,1,2
-
-                currSum = currSum - cost[currIdx] + gas[nextIdx]; // 8,7,6,5
-                currIdx = nextIdx; // 4,0,1,2
+            if(total < 0){
+                total = 0;
+                idx = i + 1;
             }
         }
-        return -1;
+        return idx;
     }
 }
