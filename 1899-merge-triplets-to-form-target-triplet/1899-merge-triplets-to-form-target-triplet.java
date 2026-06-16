2f622b82f380256cc8de395222916c5cbc5a3833
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-16 19:53:03 +0300

    Time: 6 ms (47.01%), Space: 168.8 MB (29.43%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -2,18 +2,20 @@ class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         if(triplets.length == 1) return Arrays.equals(triplets[0] , target);
 
-        int giv1 = target[0] , giv2 = target[1] , giv3 = target[2];
+        int giv1 = target[0] , giv2 = target[1] , giv3 = target[2]; // targets (given)
         int tar1 = 0 , tar2 = 0 , tar3 = 0;
+
         for(int i = 0 ; i < triplets.length ; i++){
             int num1 = triplets[i][0] , num2 = triplets[i][1] , num3 = triplets[i][2];
-            if(num1 > giv1 || num2 > giv2 || num3 > giv3) continue;
+            if(num1 > giv1 || num2 > giv2 || num3 > giv3) continue; // invalid triplet
 
+            // merging
             tar1 = Math.max(tar1 , num1);
             tar2 = Math.max(tar2 , num2);
             tar3 = Math.max(tar3 , num3);
         }
-        int[] res = new int[]{tar1 , tar2 , tar3};
-        
-        return Arrays.equals(res , target);
+
+        // check equality
+        return Arrays.equals(new int[]{tar1 , tar2 , tar3} , target);
     }
 }
