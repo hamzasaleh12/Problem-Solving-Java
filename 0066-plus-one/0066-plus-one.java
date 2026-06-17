a4e45d09eae3c3e8acda70df5ad9b494bcfb68ac
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-17 15:11:27 +0300

    Time: 0 ms (100%), Space: 43.3 MB (77.97%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length; // 1
-        if(digits[n - 1] != 9){
-            digits[n - 1]++;
-            return digits;
-        }
-        
+
         for(int j = n - 1 ; j >= 0 ; j--){
             if(digits[j] == 9) digits[j] = 0;
             else{
                 digits[j]++;
-                break;
+                return digits;
             }
         }
 
-        if(digits[0] == 0){
-            int[] newDigits = new int[n + 1];
-            newDigits[0] = 1;
-            return newDigits;
-        }
-
-        return digits;
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
