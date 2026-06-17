22d9ad1ce0f4405f620386e5940c51598510f95b
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-17 14:25:40 +0300

    Time: 0 ms (100%), Space: 42.5 MB (34.4%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-        while(true){
-            slow = helper(slow);
-            fast = helper(helper(fast));
-
-            if(fast == 1) return true;
-            else if(slow == fast) return false;
+        while (n != 1 && n != 4) {
+            n = helper(n);
         }
+        
+        return n == 1;
     }
-    private int helper(int n){
+    
+    private int helper(int n) {
         int sum = 0;
-        while(n > 0){
+        while (n > 0) {
             int num = n % 10;
             sum += num * num;
             n /= 10;
