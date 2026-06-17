7135888df2b3a58dc6bd5ca136e49430c76a1443
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-17 14:02:55 +0300

    Time: 1 ms (79.79%), Space: 42.5 MB (51.16%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,15 +1,15 @@
 class Solution {
     Set<Integer> set = new HashSet<>();
 
-    public boolean isHappy(int n) { // 19
+    public boolean isHappy(int n) {
         if(n == 1) return true;
         else if(!set.add(n)) return false;
 
         int sum = 0;
         while(n / 10 != 0){
-            int num = n % 10; // 9
-            sum += num * num;  // 81
-            n /= 10; // 1
+            int num = n % 10;
+            sum += num * num;
+            n /= 10;
         }
         sum += n * n;
         return isHappy(sum);
