7e2faafae4a38f2d6eca6f3d230f1885154e4c44
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-18 18:01:41 +0300

    Time: 3 ms (80.76%), Space: 43.9 MB (24.6%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int n1 = num1.length() , n2 = num2.length();
+        int[] res = new int[n1 + n2];
+
+        for(int j = n2 - 1 ; j >= 0 ; j--){
+            for(int i = n1 - 1 ; i >= 0 ; i--){
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+                int sum = res[i + j + 1] + mul;
+                res[i + j + 1] = sum % 10; // curr
+                res[i + j] += sum / 10; // next (carry)
+            }
+        }
+
+
+        int i = 0;
+        while(i < res.length && res[i] == 0){
+            i++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int k = i ; k < res.length ; k++){
+            sb.append(res[k]);
+        }
+
+        return (sb.toString().equals("")) ? "0" : sb.toString();
+    }
+}
