685f8a4b74fdd8d3e9df62b81f999f71b665cfe6
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-20 16:31:38 +0300

    Time: 2 ms (96.65%), Space: 48.6 MB (48.81%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,9 +1,12 @@
 class Solution {
     public int[] countBits(int n) {
         int[] res = new int[n + 1];
-        for(int i = 0; i <= n ; i++){
-           res[i] = Integer.bitCount(i); 
+        
+        for (int i = 1; i <= n; i++) {
+            // State transition: number of bits in (i/2) + 1 if 'i' is odd
+            res[i] = res[i >> 1] + (i & 1);
         }
+        
         return res;
     }
 }
