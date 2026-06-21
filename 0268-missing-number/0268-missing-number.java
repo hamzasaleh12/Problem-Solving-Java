ef371a711100d812978bf201d27ca56c029b3e77
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-21 17:17:32 +0300

    Time: 0 ms (100%), Space: 47.1 MB (90.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+
+        int tar = (n * (n + 1)) / 2;
+
+        return tar - sum;
+    }
+}
