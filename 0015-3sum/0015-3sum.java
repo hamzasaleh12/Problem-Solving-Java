234033bb10d2933dc0d823fcefeab8bec5fbcf40
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-24 14:38:31 +0300

    Time: 39 ms (21.3%), Space: 59.4 MB (32.83%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums == null) return result;
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+        int n = nums.length;
 
-        for(int i = 0 ; i < nums.length - 2 ; i++){
-            if(nums[i] > 0) break;
-
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+        for(int i = 0 ; i < n ; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue; // dub -> i
 
             int left = i + 1;
-            int right = nums.length - 1;
-            while(left < right){
-                int sum = nums[left] + nums[right] + nums[i];
+            int right = n - 1;
 
-                if(sum == 0){
-                    result.add(List.of(nums[left] , nums[i] , nums[right])); 
-                    left++;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    res.add(List.of(nums[i] , nums[left] , nums[right]));
                     right--;
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-                    while(left < right && nums[right] == nums[right + 1]) right--;
+                    left++;
+                    while(left < right && nums[right] == nums[right + 1]) right--;  // dub -> r
+                    while(left < right && nums[left] == nums[left - 1]) left++;     // dub -> l
                 }
-                else if(sum > 0) right--;
-                else left++;
+                else if(sum > 0){
+                    right--;
+                    while(left < right && nums[right] == nums[right + 1]) right--; // dub -> r
+                } else{
+                    left++;
+                    while(left < right && nums[left] == nums[left - 1]) left++; // // dub -> l
+                }
             }
         }
-        return result;
+
+        return res;
     }
 }
