6f5c37cc8a31d7fa4dace6cce72c105ab252da2d
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-24 14:57:04 +0300

    Time: 2 ms (99.59%), Space: 44.3 MB (74.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,13 +3,16 @@ class Solution {
         if(s.length() != t.length()) return false;
         int n = s.length();
 
-        int[] arrS = new int[26];
-        int[] arrT = new int[26];
+        int[] arr = new int[26];
         for(int i = 0 ; i < n ; i++){
-            arrS[s.charAt(i) - 'a']++;
-            arrT[t.charAt(i) - 'a']++;
+            arr[s.charAt(i) - 'a']++;
         }
 
-        return Arrays.equals(arrS , arrT);
+        for(int i = 0 ; i < n ; i++){
+            int idx = t.charAt(i) - 'a';
+            if(--arr[idx] < 0) return false;
+        }
+
+        return true;
     }
 }
