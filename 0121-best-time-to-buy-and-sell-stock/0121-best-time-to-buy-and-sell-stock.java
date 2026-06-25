892bb1bb1bafbe83a3824abe66e3038362885a39
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-25 15:11:29 +0300

    Time: 1 ms (99.94%), Space: 94.2 MB (90.62%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
         int min = Integer.MAX_VALUE;
 
         int maxProfit = 0;
         for(int price : prices){
             if(price < min){
-                min = price; // 2 , 1
-                max = 0;
-            } else if(price > max){
-                max = price; // 4
+                min = price;
+            } else{
+                maxProfit = Math.max(maxProfit , price - min);
             }
-
-            maxProfit = Math.max(maxProfit , max - min); // 2
         }
 
         return maxProfit;
