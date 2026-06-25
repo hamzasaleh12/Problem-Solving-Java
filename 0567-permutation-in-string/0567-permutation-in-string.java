37aebf47b936a7829fea3e1160d76f1bc393c49f
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-25 15:29:22 +0300

    Time: 6 ms (87.23%), Space: 43.7 MB (92.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1 == null || s2 == null || s1.length() > s2.length()) return false;
+        if(s1.length() > s2.length()) return false;
 
         int[] s1Arr = new int[26];
         int[] s2Arr = new int[26];
-
-        for(int i = 0; i < s1.length() ; i++){
-            s1Arr[s1.charAt(i) - 'a']++;
-            s2Arr[s2.charAt(i) - 'a']++;
+        for(int i = 0 ; i < s1.length() ; i++){
+            s1Arr[s1.charAt(i) - 'a']++; // ab
+            s2Arr[s2.charAt(i) - 'a']++; // ei
         }
 
-        if(Arrays.equals(s1Arr , s2Arr)) return true;
+        if(Arrays.equals(s1Arr , s2Arr)) return true; // f
 
         int left = 0;
-        for(int i = s1.length() ; i < s2.length() ; i++){
-            s2Arr[s2.charAt(i) - 'a']++;
+        for(int right = s1.length() ; right < s2.length() ; right++){
+            s2Arr[s2.charAt(right) - 'a']++;
             s2Arr[s2.charAt(left++) - 'a']--;
 
             if(Arrays.equals(s1Arr , s2Arr)) return true;
         }
-        
+
         return false;
     }
 }
