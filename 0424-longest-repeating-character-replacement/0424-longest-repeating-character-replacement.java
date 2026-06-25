e55ee1a59a4c54b6b940d742254a90a5508c82e4
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-25 15:35:52 +0300

    Time: 8 ms (67.71%), Space: 45.9 MB (90.23%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,18 +3,17 @@ class Solution {
         int[] arr  = new int[26];
         int maxLength = 0;
         int maxFrequent = 0;
+        
         int i = 0;
         for(int j = 0 ; j < s.length() ; j++){
             int frequent = ++arr[s.charAt(j) - 'A'];
             maxFrequent = Math.max(maxFrequent , frequent);
-            
+
             if((j - i + 1) - maxFrequent > k){
                 arr[s.charAt(i++) - 'A']--;
             }
-
-            maxLength = Math.max(maxLength , j - i + 1);
         }
 
-        return maxLength;
+        return s.length() - i;
     }
 }
