f6824812fa48881ff12f3c21442ddf42a2731c07
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-25 21:45:11 +0300

    Time: 2 ms (87.94%), Space: 49.5 MB (14.02%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,40 +10,35 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        // 1 -> 2 -> 3 -> 4 -> 5
-        // 1 -> 2 , 3 -> 4
-        ListNode slow = head; // 1
-        ListNode fast = head.next; // 2
+        ListNode slow = head;
+        ListNode fast = head;
+
         while(fast != null && fast.next != null){
-            slow = slow.next; // 2 , 3
-            fast = fast.next.next; //4 , null
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        // 1 -> 2 -------- 3 -> 4
-        // 1 -> 2 -> 3 --- 4 -> 5
-        ListNode curr = slow.next;
+
         ListNode prev = null;
+        ListNode curr = slow.next;
         slow.next = null;
         while(curr != null){
             ListNode temp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = temp;
+        } // 4 -> 3
+        
+        ListNode part1 = head; // 1 -> 2
+        ListNode part2 = prev; // 4 -> 3
+        while(part2 != null && part1 != null){
+            ListNode temp1 = part1.next; // 2 , null
+            ListNode temp2 = part2.next; // 3 , null
+
+            part1.next = part2; // 1 -> 4 , 2 -> 3
+            part2.next = temp1; // 4 -> 2 , 3 -> null
+
+            part1 = temp1; // 2 , null
+            part2 = temp2; // 3 , null
         }
-        // 1(head) -> 2(slow) -------- 4(prev) -> 3
-        // 1(head) -> 2 -> 3(slow) --- 5(prev) -> 4
-
-        ListNode fir = head; // 1 -> 2
-        ListNode sec = prev; // 4 -> 3
-        while(sec != null){
-            ListNode temp1 = fir.next; // 2 , null
-            ListNode temp2 = sec.next; // 3 , null
-
-            fir.next = sec; // 1 -> 4 -> 2 -> 3
-            sec.next = temp1; // 4 -> 2 -> 3 -> null
-
-            fir = temp1; // 2 , null
-            sec = temp2; // 3 , null
-        } 
-        // 1 -> 4 -> 2 -> 3 -> null
     }
 }
