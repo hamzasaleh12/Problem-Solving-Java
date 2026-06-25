c544686665dbda3c09dc0a19c6b9c3765d6350d5
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-25 17:38:58 +0300

    Time: 22 ms (96.54%), Space: 103.9 MB (82.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int[] result = new int[temperatures.length];
-        Deque<Integer> stack = new ArrayDeque<>();
+    public int[] dailyTemperatures(int[] temps) {
+        int n = temps.length;
+        int[] res = new int[n];
 
-        for(int i = 0 ; i < temperatures.length ; i++){
-            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
-                result[stack.peek()] = i - stack.poll();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0 ; i < n ; i++){
+            int temp = temps[i];
+
+            while(!stack.isEmpty() && temps[stack.peek()] < temp){
+                int idx = stack.pop();
+                res[idx] = i - idx;
             }
+
             stack.push(i);
         }
 
-        return result;
+        return res;
     }
 }
