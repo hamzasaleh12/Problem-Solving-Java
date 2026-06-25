58a29c158ee1d0ebebc1d52d917e99d70e21e9dd
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-06-25 18:32:58 +0300

    Time: 89 ms (29.79%), Space: 95.8 MB (75.46%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,24 +1,24 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        double[][] cars = new double[speed.length][2];
-        for(int i = 0 ; i < speed.length ; i++){
-            cars[i][0] = position[i];
-            cars[i][1] = (double) (target - position[i]) / speed[i]; // time
+        int n = position.length;
+
+        double[][] fleets = new double[n][2];
+        for(int i = 0 ; i < n ; i++){
+            fleets[i][0] =  position[i];
+            fleets[i][1] = (double) (target - position[i]) / speed[i];
         }
-        // [[10,1] , [8 , 1] , [0 , 12] , [5 , 6] , [3 , 3]]
 
-        Arrays.sort(cars , (a , b) -> Double.compare(b[0] , a[0]));
-
-        // [[10 , 1] , [8 , 1] , [5 , 6] , [3 , 3] , [0 , 12]]
-
-        int fleets = 0;
-        double maxTime = 0;
-        for(int i = 0 ; i < speed.length ; i++){
-            if(maxTime < cars[i][1]){ //  t , f , t , f , t
-                maxTime = cars[i][1]; // 1 , 6 , 12
-                fleets++; // 1 , 2 , 3
+        Arrays.sort(fleets , (a,b) -> Double.compare(b[0] , a[0]));
+        
+        int count = 1;
+        double maxTime = fleets[0][1];
+        for(int i = 1 ; i < n ; i++){
+            if(fleets[i][1] > maxTime){
+                count++;
+                maxTime =  fleets[i][1];
             }
         }
-        return fleets;
+
+        return count;
     }
 }
