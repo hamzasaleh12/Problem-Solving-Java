f5bf3ad3fe70deec9b0dd7bc6c7309b73686f4e2
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-03 12:11:47 +0300

    Time: 3 ms (66.76%), Space: 46.6 MB (33.34%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -16,16 +16,14 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root == null) return false;
-        
-        if(root.val == subRoot.val && isSameTree(root , subRoot)) return true;
-
+        if(root.val == subRoot.val && isSame(root , subRoot)) return true;
         return isSubtree(root.left , subRoot) || isSubtree(root.right , subRoot);
     }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
+    private boolean isSame(TreeNode p , TreeNode q){
+        if(p == null & q == null) return true;
         if(p == null || q == null) return false;
         if(p.val != q.val) return false;
 
-        return isSameTree(p.left , q.left) && isSameTree(p.right , q.right);
+        return isSame(p.left , q.left) && isSame(p.right , q.right);
     }
 }
