be533f46be4eafcc3236dae4267999eb3ed0f3c5
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-04 23:22:17 +0300

    Time: 23 ms (65.62%), Space: 52.2 MB (56.67%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,22 +1,24 @@
 class KthLargest {
-    private PriorityQueue<Integer> queue;
+    private PriorityQueue<Integer> pq;
     private int k;
-
+    
     public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>();
         this.k = k;
-        queue = new PriorityQueue<>();
-
         for(int num : nums){
-            add(num);
+            pq.add(num);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
     }
     
     public int add(int val) {
-        queue.add(val);
-        if(queue.size() > k){
-            queue.poll();
+        pq.add(val);
+        if(pq.size() > k){
+            pq.poll();
         }
-        return queue.peek(); 
+        return pq.peek();
     }
 }
 
