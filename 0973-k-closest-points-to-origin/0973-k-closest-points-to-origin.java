bb815a6c74f580599344eef85ffa8c28dddd8dbf
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-05 00:07:18 +0300

    Time: 46 ms (14.36%), Space: 50 MB (99.99%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[2] , a[2]));
         int n = points.length;
 
-        int[][] pointsWithDis = new int[n][3];
-        for(int i = 0 ; i < n ; i++){
-            int x = points[i][0]; pointsWithDis[i][0] = x;
-            int y = points[i][1]; pointsWithDis[i][1] = y;
-            pointsWithDis[i][2] = x * x + y * y;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((idxA, idxB) -> {
+            int distA = points[idxA][0] * points[idxA][0] + points[idxA][1] * points[idxA][1];
+            int distB = points[idxB][0] * points[idxB][0] + points[idxB][1] * points[idxB][1];
+            return Integer.compare(distB, distA); // Max-Heap
+        });
 
-            pq.add(pointsWithDis[i]);
+        for(int i = 0 ; i < n ; i++){
+            pq.add(i);
+            
             if(pq.size() > k){
                 pq.poll();
             }
@@ -17,9 +18,7 @@ class Solution {
 
         int[][] res = new int[k][2];
         for(int i = 0 ; i < k ; i++){
-            int[] curr = pq.poll();
-            res[i][0] = curr[0];
-            res[i][1] = curr[1];
+            res[i] = points[pq.poll()];
         }
 
         return res;
