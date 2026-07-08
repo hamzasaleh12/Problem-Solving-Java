72412b9ac159bcbd9428eec847527c5dbaf3091e
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-08 13:01:55 +0300

    Time: 32 ms (24.24%), Space: 48.3 MB (37.24%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -5,7 +5,7 @@ class Solution {
             arr[task - 'A']++;
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b , a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int f : arr){
             if(f != 0) pq.add(f); // [3,3]
         }
@@ -13,11 +13,13 @@ class Solution {
         int count = tasks.length; // 6
         while(!pq.isEmpty()){
             List<Integer> list = new ArrayList<>();
+
             for(int i = 0 ; i <= n ; i++){ // 0,1,2
                 if(pq.isEmpty()){ // idle
                     if(!list.isEmpty()) count++; // 8
                     continue;
                 }
+
                 int fre = pq.poll(); // 3,3
                 if(fre - 1 > 0) list.add(fre - 1);
             }
