60fbab81e30c433f21dde051b7f2ae5911d31866
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-08 13:13:03 +0300

    Time: 3 ms (82.62%), Space: 48.4 MB (22.45%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,33 +1,15 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] arr = new int[26];
+
+        int max = 0;
         for(char task : tasks){
-            arr[task - 'A']++;
+            max = Math.max(max ,++arr[task - 'A']);
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int f : arr){
-            if(f != 0) pq.add(f); // [3,3]
-        }
+        int countMaxFre = 0;
+        for(int a : arr) if(a == max) countMaxFre++;
 
-        int count = tasks.length; // 6
-        while(!pq.isEmpty()){
-            List<Integer> list = new ArrayList<>();
-
-            for(int i = 0 ; i <= n ; i++){ // 0,1,2
-                if(pq.isEmpty()){ // idle
-                    if(!list.isEmpty()) count++; // 8
-                    continue;
-                }
-
-                int fre = pq.poll(); // 3,3
-                if(fre - 1 > 0) list.add(fre - 1);
-            }
-
-            for(int num : list){
-                pq.add(num); // [A,B]
-            }
-        }
-        return count;
+        return Math.max(tasks.length , (max - 1) * (n + 1) + countMaxFre);
     }
 }
