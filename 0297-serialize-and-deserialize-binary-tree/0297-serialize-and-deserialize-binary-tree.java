031883bed42e97d4876a52528b961c03994e9c84
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-08 12:15:12 +0300

    Time: 15 ms (49.24%), Space: 47.8 MB (68.26%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -8,57 +8,57 @@
  * }
  */
 public class Codec {
+    Queue<TreeNode> queue = new LinkedList<>();
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) return "null";
         StringBuilder str = new StringBuilder();
 
         queue.add(root);
         while(!queue.isEmpty()){
-            TreeNode temp = queue.poll();
-            if(temp == null){
-                str.append("null,");
+            TreeNode curr = queue.poll();
+
+            if(curr == null){
+                str.append("null").append(",");
                 continue;
             }
-            str.append(temp.val).append(",");
-            queue.add(temp.left);
-            queue.add(temp.right);
+
+            str.append(curr.val).append(",");
+
+            queue.add(curr.left);
+            queue.add(curr.right);
         }
         return str.toString();
     }
+    // "1,2,3,null,null,4,5,null,null"
 
     // Decodes your encoded data to tree.
-    // [1,2,3,null.null,4,5,null.null]
     public TreeNode deserialize(String data) {
-        if(data.equals("null,")) return null;
+        if(data.equals("null")) return null;
 
-        String[] strs = data.split(",");
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        TreeNode root = new TreeNode(Integer.parseInt(strs[0]));
+        String[] tokens = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(tokens[0]));
         queue.add(root);
 
         int i = 1;
-        while(!queue.isEmpty()){
+        while(i < tokens.length && !queue.isEmpty()){
             TreeNode parent = queue.poll();
 
-            if(!strs[i].equals("null")) {
-                TreeNode leftNode = new TreeNode(Integer.parseInt(strs[i]));
-                parent.left = leftNode;
-                queue.add(leftNode);
-            }
+            parent.left = (tokens[i].equals("null")) ? null : new TreeNode(Integer.parseInt(tokens[i]));
+            if(parent.left != null) queue.add(parent.left);
             i++;
-            
-            if(!strs[i].equals("null")) {
-                TreeNode rightNode = new TreeNode(Integer.parseInt(strs[i]));
-                parent.right = rightNode;
-                queue.add(rightNode);
+
+            if(i < tokens.length){
+                parent.right = (tokens[i].equals("null")) ? null : new TreeNode(Integer.parseInt(tokens[i]));
+                if(parent.right != null) queue.add(parent.right);
             }
             i++;
         }
         return root;
     }
 }
+
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
