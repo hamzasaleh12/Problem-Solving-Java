edfbd22f427f7e47e7162e2eb75b6c8283328971
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-08 12:55:15 +0300

    Time: 33 ms (22.44%), Space: 47.9 MB (89.2%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -5,26 +5,25 @@ class Solution {
             map.put(task , map.getOrDefault(task,0) + 1);
         }
 
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> Integer.compare(map.get(b) , map.get(a)));
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b , a));
         for(char ch : map.keySet()){
-            pq.add(ch); // [A,B]
+            pq.add(map.get(ch)); // [3,3]
         }
 
         int count = tasks.length; // 6
         while(!pq.isEmpty()){
-            List<Character> list = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
             for(int i = 0 ; i <= n ; i++){ // 0,1,2
                 if(pq.isEmpty()){ // idle
                     if(!list.isEmpty()) count++; // 8
                     continue;
                 }
-                char currChar = pq.poll(); // A,B
-                map.put(currChar , map.get(currChar) - 1); // [A:0 , B:0]
-                if(map.get(currChar) > 0) list.add(currChar); // []
+                int fre = pq.poll(); // 3,3
+                if(fre - 1 > 0) list.add(fre - 1);
             }
 
-            for(char ch : list){
-                pq.add(ch); // [A,B]
+            for(int num : list){
+                pq.add(num); // [A,B]
             }
         }
         return count;
