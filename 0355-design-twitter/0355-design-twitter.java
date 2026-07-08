3adc5be771733b6ac222c361da9d7f9c4e9c3f67
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-08 17:40:31 +0300

    Time: 30 ms (86.77%), Space: 56.5 MB (49.99%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,74 +1,52 @@
 class Twitter {
-    class Tweet{
-        int id;
-        int count;
-        Tweet next;
-        public Tweet(int id , int count){
-            this.id = id;
-            this.count = count;
-            this.next = null;
-        }
-    }
-
-    Map<Integer , Set<Integer>> flrs;
-
-    static int timestamp = 0;
-    Map<Integer,Tweet> map;
+    // userId -> [tweets]
+    // userId -> [fols]
+    Map<Integer,List<int[]>> tweets; // id -> [ [id,time] , [...] , ...]
+    Map<Integer,Set<Integer>> fols; // id -> [ [] , [] ]
+    private int timestamp = 0;
 
     public Twitter() {
-        flrs = new HashMap<>();
-        map = new HashMap<>();
+        tweets = new HashMap<>();
+        fols = new HashMap<>();
     }
     
     public void postTweet(int userId, int tweetId) {
-        follow(userId , userId);
-
-        Tweet newTweet = new Tweet(tweetId , timestamp++);
-        newTweet.next = map.get(userId);
-
-        map.put(userId , newTweet);
+        follow(userId , userId); // follow himself
+        tweets.computeIfAbsent(userId , k -> new ArrayList<>()).add(new int[]{tweetId , timestamp++});
     }
     
     public List<Integer> getNewsFeed(int userId) {
+        if(fols.get(userId) == null) return new ArrayList<>();
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(b[1] , a[1]));
+        Set<Integer> followers = fols.get(userId);
+
+        for(int f : followers){ // keys
+            if(tweets.get(f) == null) continue;
+
+            List<int[]> tweetsForFriend = tweets.get(f);
+            for(int i = tweetsForFriend.size() - 1 ; i >= 0 ; i--){
+                pq.add(tweetsForFriend.get(i));
+            }
+        }
+
+        int count = 0;
         List<Integer> res = new ArrayList<>();
-
-        PriorityQueue<Tweet> pq = new PriorityQueue<>((a,b) -> Integer.compare(b.count , a.count));
-        Set<Integer> flsId =  flrs.get(userId);
-
-        if(flsId != null){
-        for(int flId : flsId){
-            Tweet head = map.get(flId);
-            if(head != null){
-                pq.add(head);
-            }
-        }
-        }
-
-        int n = 0;
-        while(!pq.isEmpty() && n < 10){
-            Tweet head = pq.poll();
-            n++;
-            res.add(head.id);
-
-            if(head.next != null){
-                pq.add(head.next);
-            }
+        while(!pq.isEmpty() && count++ < 10){
+            int[] tweetAndtime = pq.poll();
+            res.add(tweetAndtime[0]);
         }
 
         return res;
     }
     
     public void follow(int followerId, int followeeId) {
-        flrs.computeIfAbsent(followerId , k -> new HashSet<>()).add(followeeId);
+        fols.computeIfAbsent(followerId , k -> new HashSet<>()).add(followeeId); // o(1)
     }
     
     public void unfollow(int followerId, int followeeId) {
-        if(followerId == followeeId) return;
-        Set<Integer> set = flrs.get(followerId);
-
-        if(set != null){
-            set.remove(followeeId);
-        }
+        if(fols.get(followerId) == null) return;
+        fols.get(followerId).remove(followeeId); // o(1)
     }
 }
 
