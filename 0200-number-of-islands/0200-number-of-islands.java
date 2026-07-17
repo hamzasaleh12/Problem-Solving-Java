bc83683a0c77649ce5beca4c32becac842875f1a
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-17 16:00:52 +0300

    Time: 3 ms (87.97%), Space: 52.4 MB (46.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,25 @@
 class Solution {
-    private static final int[][] dcs = {{1,0} , {0,1} , {-1,0} , {0,-1}};
-
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-
-        int islands = 0;
+        int count = 0;
         for(int i = 0 ; i < grid.length ; i++){
-            for(int j = 0 ; j < grid[i].length ; j++){
+            for(int j = 0 ; j < grid[0].length ; j++){
                 if(grid[i][j] == '1'){
-                    dfs(i , j , grid);
-                    islands++;
+                    count++;
+                    dfs(grid , i , j);
                 }
             }
         }
-        return islands;
+
+        return count;
     }
-    private void dfs(int r , int c , char[][] grid){
-        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0') return;
+
+    private void dfs(char[][] grid , int row , int col){
+        if(row == grid.length || row < 0 || col == grid[0].length || col < 0 || grid[row][col] == '0') return; // base case
         
-        grid[r][c] = '0';
-        for(int i = 0 ; i < 4 ; i++){
-            dfs(r + dcs[i][0] , c + dcs[i][1] , grid);
-        }
+        grid[row][col] = '0';
+        dfs(grid , row + 1 , col);
+        dfs(grid , row - 1 , col);
+        dfs(grid , row , col + 1);
+        dfs(grid , row , col - 1);
     }
 }
