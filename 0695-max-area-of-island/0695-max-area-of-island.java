a4bd1bbb5990875ab9dec3d5c17b82da5fbb2b54
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-17 16:27:18 +0300

    Time: 2 ms (63.85%), Space: 46.7 MB (22.76%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,29 +1,27 @@
 class Solution {
-    private static final int[][] drs = {{0,1} , {0,-1} , {1,0} , {-1,0}};
-
+    private static int[][] drs = {{0,1} , {1,0} , {-1 , 0} , {0 , -1}};
+    
     public int maxAreaOfIsland(int[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-
         int max = 0;
         for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0 ; j < grid[0].length ; j++){
                 if(grid[i][j] == 1){
-                    max = Math.max(max , dfs(i , j , grid));
+                    max = Math.max(max , dfs(grid , i , j));
                 }
             }
         }
 
         return max;
     }
-    private int dfs(int r , int c , int[][] grid){
-        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) return 0;
-        grid[r][c] = 0;
-
-        int area = 1;
-        for(int i = 0; i < 4 ; i++){
-            area += dfs(r + drs[i][0] , c + drs[i][1] , grid);
-        }
+    private int dfs(int[][] grid , int row , int col){
+        if(row == grid.length || row < 0 || col == grid[0].length || col < 0 || grid[row][col] == 0) return 0; // base case
+        grid[row][col] = 0;
         
+        int area = 1;
+        for(int[] d : drs){
+            area += dfs(grid , row + d[0] , col + d[1]);
+        }
+
         return area;
     }
 }
