ebe367e96a4eed7c681e7214ef2f5257b9aef638
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-17 18:02:49 +0300

    Time: 4 ms (98.85%), Space: 47.8 MB (18.76%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,37 +1,43 @@
 class Solution {
-    private static final int[][] drs = {{1,0} , {0,1} , {-1,0} , {0,-1}};
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> result = new ArrayList<>();
+    private static int[][] drs = {{0,1} , {1,0} , {-1,0} , {0,-1}};
 
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int rows = heights.length;
         int cols = heights[0].length;
-        boolean[][] pac = new boolean[rows][cols];
-        boolean[][] atl = new boolean[rows][cols];
 
-        for(int i = 0 ; i < rows ; i++){
-            dfs(i , 0 , heights[i][0] , heights , pac);
-            dfs(i , cols - 1 , heights[i][cols - 1] , heights , atl);
-        }
+        // Fir col & Fir row --> Pacific Ocean
+        boolean[][] pacific = new boolean[rows][cols];
+        // Las col & Las row --> Atlantic Ocean
+        boolean[][] atlantic = new boolean[rows][cols];
+
+        // Fir & Last row
         for(int i = 0 ; i < cols ; i++){
-            dfs(0 , i , heights[0][i] , heights , pac);
-            dfs(rows - 1 , i , heights[rows - 1][i] , heights , atl);
+            helper(heights , pacific , 0 , i , heights[0][i]);
+            helper(heights , atlantic , rows - 1 , i , heights[rows - 1][i]);
         }
 
-        for(int i = 0; i < rows ; i++){
+        // Fir & Last col
+        for(int i = 0 ; i < rows ; i++){
+            helper(heights , pacific , i , 0 , heights[i][0]);
+            helper(heights , atlantic , i , cols - 1 , heights[i][cols - 1]);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0 ; i < rows ; i++){
             for(int j = 0 ; j < cols ; j++){
-                if(pac[i][j] && atl[i][j]) result.add(List.of(i,j));
+                if(pacific[i][j] && atlantic[i][j]) res.add(List.of(i , j));
             }
         }
 
-        return result;
+        return res;
     }
-    private void dfs(int row , int col , int curr , int[][] heights , boolean[][] visited){
-        if(row < 0 || row >= visited.length || col < 0 || col >= visited[0].length || 
-        visited[row][col] || heights[row][col] < curr) return;
 
-        visited[row][col] = true;
+    private void helper(int[][] heights , boolean[][] ochean , int r , int c , int min){
+        if(r < 0 || r >= heights.length || c < 0 || c >= heights[0].length || ochean[r][c] || heights[r][c] < min) return;
+        ochean[r][c] = true;
+
         for(int[] d : drs){
-            dfs(row + d[0] , col + d[1] , heights[row][col] , heights , visited);
+            helper(heights , ochean , r + d[0] , c + d[1] , heights[r][c]);
         }
     }
 }
