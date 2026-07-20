5192841d0e48e561630487c07ce45c08aad18ddb
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-20 14:32:11 +0300

    Time: 16 ms (99.2%), Space: 44.9 MB (96.72%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,27 +1,31 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
+        int l = points.length;
+        boolean[] visited = new boolean[l];
 
-        boolean[] visisted = new boolean[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1] , b[1]));
-        pq.add(new int[]{0,0});
+        int[] minDis = new int[l];
+        Arrays.fill(minDis , Integer.MAX_VALUE);
+        minDis[0] = 0;
 
-        int sum = 0;
-        int totalEdges = 0; 
-        while(!pq.isEmpty() && totalEdges < n){
-            int[] curr = pq.poll();
-            int x = curr[0] , val = curr[1];
-
-            if(visisted[x]) continue; // cycle
-
-            visisted[x] = true;
-            for(int i = 0; i < n ; i++){
-                if(visisted[i]) continue;
-                pq.add(new int[]{i , Math.abs(points[x][0] - points[i][0]) + Math.abs(points[x][1] - points[i][1])});
+        int tarSum = 0;
+        for(int i = 0 ; i < l ; i++){
+            int min = Integer.MAX_VALUE , idx = Integer.MAX_VALUE;
+            for(int j = 0 ; j < l ; j++){
+                if(!visited[j] && minDis[j] < min) {
+                    min = minDis[j];
+                    idx = j;
+                }
             }
-            totalEdges++;
-            sum += val;
+
+            if(min == Integer.MAX_VALUE) break;
+
+            visited[idx] = true;
+            tarSum += min;
+            for(int j = 0 ; j < l ; j++){
+                if(!visited[j]) minDis[j] = Math.min(minDis[j] , Math.abs(points[idx][0] - points[j][0]) +
+                Math.abs(points[idx][1] - points[j][1]));
+            }
         }
-        return sum;
+        return tarSum;
     }
 }
