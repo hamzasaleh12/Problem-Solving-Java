b74d28fe9fcd1f4d25d36b1f7e9a710e04cc5085
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-20 14:26:49 +0300

    Time: 75 ms (71.73%), Space: 76.9 MB (57.24%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,52 +1,27 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        List<int[]> edges = new ArrayList<>();
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                int dis = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                edges.add(new int[]{i , j , dis});
-            }
-        }
-
-        edges.sort((a, b) -> Integer.compare(a[2], b[2]));
-        
-        // DSU
-        int[] parent = new int[n];
-        for(int i = 1 ; i < n ; i++) parent[i] = i;
-
-        int[] rank = new int[n];
+        boolean[] visisted = new boolean[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1] , b[1]));
+        pq.add(new int[]{0,0});
 
         int sum = 0;
-        int edgesCount = 0;
-        for(int[] edge : edges){
-            if(edgesCount == n - 1) break;
+        int totalEdges = 0; 
+        while(!pq.isEmpty() && totalEdges < n){
+            int[] curr = pq.poll();
+            int x = curr[0] , val = curr[1];
 
-            int u = find(edge[0] , parent);
-            int v = find(edge[1] , parent);
+            if(visisted[x]) continue; // cycle
 
-            if(u == v) continue; // Cycle
-
-            union(u , v , parent , rank);
-            sum += edge[2];
-            edgesCount++;
+            visisted[x] = true;
+            for(int i = 0; i < n ; i++){
+                if(visisted[i]) continue;
+                pq.add(new int[]{i , Math.abs(points[x][0] - points[i][0]) + Math.abs(points[x][1] - points[i][1])});
+            }
+            totalEdges++;
+            sum += val;
         }
-
         return sum;
-    }
-    private int find(int val , int[] parent){
-        if(val == parent[val]) return val;
-        return parent[val] = find(parent[val] , parent);
-    }
-    private void union(int fir , int sec , int[] parent , int[] rank){
-        if(rank[fir] > rank[sec]){
-            parent[sec] = fir;
-        } else if(rank[sec] > rank[fir]){
-            parent[fir] = sec;
-        } else{
-            parent[sec] = fir;
-            rank[fir]++;
-        }
     }
 }
