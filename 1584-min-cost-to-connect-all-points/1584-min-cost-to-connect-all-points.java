1bb05f3a219fe791acb722ca5f38684f7ad7e73c
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-20 13:01:10 +0300

    Time: 360 ms (39.76%), Space: 98.5 MB (28.07%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,35 +1,52 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        if(points == null || points.length == 0) return 0;
+        int n = points.length;
+        List<int[]> edges = new ArrayList<>();
 
-        int l = points.length; int w = points[0].length;
-        boolean[] visited = new boolean[l];
-
-        int[] minDis = new int[l];
-        Arrays.fill(minDis , Integer.MAX_VALUE);
-        minDis[0] = 0;
-
-        int tarSum = 0;
-        for(int i = 0 ; i < l ; i++){
-            int min = Integer.MAX_VALUE; int idx = Integer.MAX_VALUE;
-            for(int j = 0 ; j < l ; j++){
-                if(!visited[j] && minDis[j] < min) {
-                    min = minDis[j];
-                    idx = j;
-                }
-            }
-
-            if(min == Integer.MAX_VALUE) break;
-
-            visited[idx] = true;
-            tarSum += min;
-            for(int j = 0 ; j < l ; j++){
-                if(!visited[j]) minDis[j] = Math.min(minDis[j] , dis(idx , j , points));
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                int dis = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[]{i , j , dis});
             }
         }
-        return tarSum;
+
+        edges.sort((a, b) -> Integer.compare(a[2], b[2]));
+        
+        // DSU
+        int[] parent = new int[n];
+        for(int i = 1 ; i < n ; i++) parent[i] = i;
+
+        int[] rank = new int[n];
+
+        int sum = 0;
+        int edgesCount = 0;
+        for(int[] edge : edges){
+            if(edgesCount == n - 1) break;
+
+            int u = find(edge[0] , parent);
+            int v = find(edge[1] , parent);
+
+            if(u == v) continue; // Cycle
+
+            union(u , v , parent , rank);
+            sum += edge[2];
+            edgesCount++;
+        }
+
+        return sum;
     }
-    private int dis(int i , int j , int[][] points){
-        return Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+    private int find(int val , int[] parent){
+        if(val == parent[val]) return val;
+        return parent[val] = find(parent[val] , parent);
+    }
+    private void union(int fir , int sec , int[] parent , int[] rank){
+        if(rank[fir] > rank[sec]){
+            parent[sec] = fir;
+        } else if(rank[sec] > rank[fir]){
+            parent[fir] = sec;
+        } else{
+            parent[sec] = fir;
+            rank[fir]++;
+        }
     }
 }
