2d42929716b804edea2cd8909eb173e748e98d69
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-21 19:53:35 +0300

    Time: 8 ms (28.7%), Space: 46.3 MB (92.89%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,31 +1,29 @@
 class Solution {
-    record Data(int curr , int path , int k){};
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         Map<Integer,List<int[]>> map = new HashMap<>();
-        for(int[] flight : flights){
-            map.computeIfAbsent(flight[0] , key -> new ArrayList<>()).add(new int[] {flight[1] , flight[2]}); // 0 : 1,100
+        for(int[] f : flights){
+            map.computeIfAbsent(f[0] , a -> new ArrayList<>()).add(new int[]{f[1] , f[2]});
         }
 
-        PriorityQueue<Data> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.path , b.path));
-        pq.add(new Data(src,0,k));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1] , b[1])); // least price
+        pq.add(new int[]{src , 0 , 0});
 
         int[] minStops = new int[n];
-        Arrays.fill(minStops, -1);
+        Arrays.fill(minStops , Integer.MAX_VALUE);
 
         while(!pq.isEmpty()){
-            Data data = pq.poll();
-            int u = data.curr , price = data.path , stops = data.k;
+            int[] curr = pq.poll(); // [0,0,0]
+            int u = curr[0] , price = curr[1] , stops = curr[2];
 
-            if(u == dst) return data.path;
-            if(stops < 0 || stops <= minStops[u]) continue;
-
+            if(u == dst) return price;
+            if(stops > minStops[u] || stops > k) continue;
             minStops[u] = stops;
 
-            if (!map.containsKey(u)) continue;
-            for(int[] neighbor : map.get(u)){
-                pq.add(new Data(neighbor[0], price + neighbor[1] , stops - 1));
+            for(int[] ne : map.getOrDefault(u , new ArrayList<>())){
+                pq.add(new int[]{ne[0] , price + ne[1] , stops + 1}); // [1,100,0]
             }
         }
+
         return -1;
     }
 }
