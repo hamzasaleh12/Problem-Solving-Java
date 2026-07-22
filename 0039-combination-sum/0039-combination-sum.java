9e0adc8ba507eb45a1d9d9fb12add88eb0edc8a0
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-22 22:27:55 +0300

    Time: 4 ms (13.71%), Space: 46.1 MB (10.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,21 @@
 class Solution {
-    List<Integer> subset = new ArrayList<>();
-    List<List<Integer>> res = new ArrayList<>();
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        dfs(0 , candidates , target , 0);
+        dfs(0 , 0 , candidates , target , new ArrayList<>() , res);
         return res;
     }
-    private void dfs(int i , int[] nums , int target , int val){
-        if(i >= nums.length || val >= target){
-            if(val == target) res.add(new ArrayList<>(subset));
+    private void dfs(int i , int sum , int[] nums , int target , List<Integer> curr , List<List<Integer>> res){
+        if(sum > target || i >= nums.length) return;
+        if(sum == target){
+            res.add(new ArrayList<>(curr));
             return;
         }
-        if(val + nums[i] > target) return;
 
-        subset.add(nums[i]);
-        dfs(i , nums , target , val + nums[i]);
-
-        subset.removeLast();
-        dfs(i + 1 , nums , target , val);
+        curr.add(nums[i]); // 2 2 2 2
+        dfs(i , sum + nums[i] , nums , target , curr , res);
+        
+        curr.removeLast();
+        dfs(i + 1 , sum , nums , target , curr , res);
     }
 }
