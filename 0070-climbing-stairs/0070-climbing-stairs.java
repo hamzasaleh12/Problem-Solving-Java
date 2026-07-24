ed04704f7eb35abdfdc154bcdb700bcc1850c7f3
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-24 19:58:08 +0300

    Time: 0 ms (100%), Space: 42.1 MB (54.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,7 +3,7 @@ class Solution {
         int[] count = {0};
 
         int[] arr = new int[n + 1];
-        Arrays.fill(arr , -1);
+        // Arrays.fill(arr , -1);
 
         return dfs(0 , n , arr);
     }
@@ -11,7 +11,7 @@ class Solution {
         if(sum == n) return 1;
         if(sum > n) return 0;
 
-        if(arr[sum] != -1) return arr[sum];
+        if(arr[sum] != 0) return arr[sum];
 
         return arr[sum] = dfs(sum + 1 , n , arr) + dfs(sum + 2 , n , arr);
     }
