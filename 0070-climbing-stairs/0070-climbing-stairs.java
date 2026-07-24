7429e5151a08b2487a5f82dad10ab00f938aa47b
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-24 20:26:59 +0300

    Time: 0 ms (100%), Space: 41.7 MB (97.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] count = {0};
-
         int[] arr = new int[n + 1];
-        // Arrays.fill(arr , -1);
-
-        return dfs(0 , n , arr);
+        arr[n] = 1 ; arr[n - 1] = 1;
+        for(int i = n - 2 ; i >= 0 ; i--){
+            arr[i] = arr[i + 1] + arr[i + 2];
+        }
+        return arr[0];
     }
     private int dfs(int sum , int n , int[] arr){
         if(sum == n) return 1;
