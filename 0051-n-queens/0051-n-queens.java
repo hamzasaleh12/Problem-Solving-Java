021f8afe1000c85ef519424316f5d71b9085a103
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-24 16:44:59 +0300

    Time: 4 ms (26.96%), Space: 46.7 MB (28.87%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,40 +1,41 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<String> part = new ArrayList<>();
-        List<List<String>> res = new ArrayList<>();
-        boolean[] visited = new boolean[n];
-        boolean[] diag1 = new boolean[n * 2];
-        boolean[] diag2 = new boolean[n * 2];
+    List<String> curr = new ArrayList<>();
+    List<List<String>> res = new ArrayList<>();
 
-        backTrack(0 , n , part , res , visited , diag1 , diag2);
+    public List<List<String>> solveNQueens(int n) {
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[n];
+        
+        Set<Integer> dia1 = new HashSet<>();
+        Set<Integer> dia2 = new HashSet<>();
+
+        char[][] board = new char[n][n];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                board[i][j] = '.';
+            }
+        }
+
+        dfs(board , 0 , row , col , dia1 , dia2 , n);
         return res;
     }
-    private void backTrack(int row , int n , List<String> part , List<List<String>> res , boolean[] visited , boolean[] diag1 , boolean[] diag2){
-        if(part.size() == n){
-            res.add(new ArrayList<>(part));
+    private void dfs(char[][] board , int r , boolean[] row , boolean[] col , Set<Integer> dia1 , Set<Integer> dia2 , int n){
+        if(r >= n){ // success
+            res.add(new ArrayList<>(curr));
             return;
         }
-        for(int col = 0 ; col < n ; col++){
-            int main = row + col;
-            int anti = row - col + (n - 1);
 
-            if(!visited[col] && !diag1[main] && !diag2[anti]){
-                visited[col] = true;
-                diag1[main] = true;
-                diag2[anti] = true;
+        for(int c = 0 ; c < n ; c++){ // column number
+            if(row[r] || col[c] || dia1.contains(r - c) || dia2.contains(r + c)) continue;
+            row[r] = true; col[c] = true; dia1.add(r - c); dia2.add(r + c);
+            
+            board[r][c] = 'Q';
+            curr.add(new String(board[r])); // [.Q..]
+            dfs(board , r + 1 , row , col , dia1 , dia2 , n);
 
-                char[] charArr = new char[n];
-                Arrays.fill(charArr , '.');
-                charArr[col] = 'Q';
-                part.add(new String(charArr));
-
-                backTrack(row + 1, n , part , res , visited , diag1 , diag2);
-
-                part.removeLast();
-                visited[col] = false;
-                diag1[main] = false;
-                diag2[anti] = false;
-            }
+            board[r][c] = '.';
+            curr.removeLast();
+            row[r] = false; col[c] = false; dia1.remove(r - c); dia2.remove(r + c);
         }
     }
 }
