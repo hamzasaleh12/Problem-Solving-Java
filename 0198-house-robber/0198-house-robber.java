bcce9a09e0af604cfc4c9bf86c19c1b71a79009a
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-26 18:34:57 +0300

    Time: 0 ms (100%), Space: 42.8 MB (57.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
+        int n = nums.length;
 
-        int[] maxRobs = new int[nums.length];
+        int[] maxRobs = new int[n];
         Arrays.fill(maxRobs , -1);
 
-        return Math.max(dfs(0 , nums , maxRobs) , dfs(1 , nums , maxRobs));
+        return dfs(0 , nums , maxRobs);
     }
     private int dfs(int start , int[] nums , int[] maxRobs){
         if(start >= nums.length) return 0;
         if(maxRobs[start] != -1) return maxRobs[start];
 
-        return maxRobs[start] = nums[start] + Math.max(dfs(start + 2 , nums , maxRobs) , dfs(start + 3 , nums , maxRobs));
+        return maxRobs[start] = Math.max(nums[start] + dfs(start + 2 , nums , maxRobs) , dfs(start + 1 , nums , maxRobs));
     }
 }
