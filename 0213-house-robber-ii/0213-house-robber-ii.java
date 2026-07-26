caec0e1f4fef3b7b5483df271f743d76c536f2b5
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-26 18:36:52 +0300

    Time: 0 ms (100%), Space: 42.9 MB (42.16%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -9,18 +9,12 @@ class Solution {
         int[] maxRobs2 = new int[n];
         Arrays.fill(maxRobs2 , -1);
 
-        int range1 = Math.max(dfs(0, nums, maxRobs1, n - 1), 
-                              dfs(1, nums, maxRobs1, n - 1));
-
-        int range2 = Math.max(dfs(1, nums, maxRobs2, n), 
-                              dfs(2, nums, maxRobs2, n));
-
-        return Math.max(range1, range2);
+        return Math.max(dfs(0, nums, maxRobs1, n - 1), dfs(1, nums, maxRobs2, n));
     }
     private int dfs(int start , int[] nums , int[] maxRobs , int length){
         if(start >= length) return 0;
         if(maxRobs[start] != -1) return maxRobs[start];
 
-        return maxRobs[start] = nums[start] + Math.max(dfs(start + 2 , nums , maxRobs , length) , dfs(start + 3 , nums , maxRobs , length));
+        return maxRobs[start] = Math.max(nums[start] + dfs(start + 2 , nums , maxRobs , length) , dfs(start + 1 , nums , maxRobs , length));
     }
 }
