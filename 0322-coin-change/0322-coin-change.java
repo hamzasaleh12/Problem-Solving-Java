a66aa339e95b03c73fd821d4a14afbfef76253fe
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-27 21:19:41 +0300

    Time: 27 ms (26.88%), Space: 47 MB (29.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
-
-        Arrays.sort(coins);
-        int n = coins.length;
-
-        Long[][] memo = new Long[n][amount + 1];
-
-        long sol = dfs(0 , coins , amount , memo);
-        return sol >= Integer.MAX_VALUE ? -1 : (int) sol;
+        int[] memo = new int[amount + 1];
+        Arrays.fill(memo, -1);
+        
+        int res = dfs(amount, coins, memo);
+        
+        return res >= 1e9 ? -1 : res;
     }
-    private long dfs(int i , int[] coins , int rem , Long[][] memo){ // 
-        if(rem == 0) return 0;
-        if(i >= coins.length || rem < 0) return Integer.MAX_VALUE;
-        if(memo[i][rem] != null) return memo[i][rem];
 
-        return memo[i][rem] = Math.min(1 + dfs(i , coins , rem - coins[i] , memo) , dfs(i + 1 , coins , rem , memo));
+    private int dfs(int rem, int[] coins, int[] memo) {
+        if (rem == 0) return 0;
+        if (rem < 0) return (int) 1e9;
+        if (memo[rem] != -1) return memo[rem];
+
+        int minCoins = (int) 1e9;
+        for (int coin : coins) {
+            minCoins = Math.min(minCoins, 1 + dfs(rem - coin, coins, memo));
+        }
+
+        return memo[rem] = minCoins;
     }
 }
