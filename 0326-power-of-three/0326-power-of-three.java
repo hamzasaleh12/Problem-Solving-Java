5df70340dd0dfcfa1451be3bc167c0332e9c8da5
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-07-29 21:32:47 +0300

    Time: 8 ms (92.59%), Space: 46.3 MB (10.58%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n == 1) return true;
-        if(n <= 0 || n % 3 != 0) return false;
-
-        return isPowerOfThree(n / 3); // 1
+        while (n > 0 && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
     }
 }
