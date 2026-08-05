09affda955120085d683a1090facd0ec3bd45694
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-08-05 16:08:28 +0300

    Time: 7 ms (72.23%), Space: 49.9 MB (25.11%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String , List<String>> map = new HashMap<>();
+        Map<String,List<String>> map = new HashMap<>(); // space = o(n) , time = 
+        for(String str : strs){ // o(n)
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr); // o(k log(k))
+            String key = new String(arr); // aet
 
-        for(String word : strs){
-            char[] wordArr = word.toCharArray();
-            Arrays.sort(wordArr);
-            String key = new String(wordArr);
-
-            map.computeIfAbsent(key , k -> new ArrayList<>()).add(word);
+            map.computeIfAbsent(key , k -> new ArrayList<>()).add(str);
         }
-
         return new ArrayList<>(map.values());
     }
 }
