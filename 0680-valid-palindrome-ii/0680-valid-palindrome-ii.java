971eed80862a4b79e5927ebb2993d7ebb7acb165
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-08-23 21:46:15 +0300

    Time: 4 ms (99.04%), Space: 47.9 MB (29.19%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isStrictPalindrome(s, left + 1, right) || 
+                       isStrictPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    private boolean isStrictPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
