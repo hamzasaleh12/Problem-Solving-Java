5d229eea2aecde391e6bbde092303d72aa14fabc
Hamza Muhammad Muhammad Eid Ali <hamzasaleh102938@gmail.com>
2026-09-11 17:59:55 +0300

    Time: 7 ms (34.93%), Space: 43.8 MB (96.13%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -5,11 +5,13 @@ class Solution {
         int[] f = new int[10];
         for(int d : digits) f[d]++;
 
-        for(int i = 1 ; i < 10 ; i++){ if(f[i] == 0) continue; // fir -> [1:9]
-            for(int j = 0 ; j < 10 ; j++){ if(f[j] == 0) continue; // sec -> [0:9]
-                for(int k = 0 ; k <= 8 ; k+=2){ if(f[k] == 0) continue;// thd -> [2,4,6,8]
+        for(int i = 1 ; i < 10 ; i++){
+            for(int j = 0 ; j < 10 ; j++){
+                for(int k = 0 ; k <= 8 ; k+=2){
                     f[i]--; f[j]--; f[k]--;
+
                     if(f[i] >= 0 && f[j] >= 0 && f[k] >= 0) res++;
+                    
                     f[i]++; f[j]++; f[k]++;
                 }
             }
